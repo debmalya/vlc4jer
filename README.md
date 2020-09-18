a0b1cf4cd526b6c8b9d46fa185c2c9a810e4eb73
Debmalya Jash <debmalya.jash@gmail.com>
2020-09-19 00:14:36 +0800

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vlc4jer
 How to use vlc4j 
+
+## Errors
+* Exception in thread "main" java.lang.UnsatisfiedLinkError: Unable to load library 'libvlc':
+
+## Reference
+* Tutorial(https://capricasoftware.co.uk/projects/vlcj-4/tutorials)
---
+++ src/main/java/org/deb/Tutorial.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.deb;
+
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
+/**
+ * @author Debmalya
+ *
+ */
+public class Tutorial {
+	public static void main(String[] args){
+//		MediaPlayerFactory factory = new MediaPlayerFactory();
+		EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+	}
+
+}
