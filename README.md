df532ab8e78a9504150eaa7e1679649806a6acbf
Debmalya Jash <debmalya.jash@gmail.com>
2020-09-19 20:17:30 +0800

    Code is working in windows@

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 How to use vlc4j 
 
 ## Errors
-* Exception in thread "main" java.lang.UnsatisfiedLinkError: Unable to load library 'libvlc':
+* Exception in thread "main" java.lang.UnsatisfiedLinkError: Unable to load library 'libvlc': As I wrongly installed 32 bit vlc, after uninstalling that version and installing 64 bit vlc solved the issue.
 
 ## Reference
 * [Tutorial](https://capricasoftware.co.uk/projects/vlcj-4/tutorials)
--- src/main/java/org/deb/Tutorial.java
+++ src/main/java/org/deb/Tutorial.java
@@ -3,14 +3,8 @@
  */
 package org.deb;
 
-import java.io.File;
-
-import uk.co.caprica.vlcj.binding.LibVlc;
-import uk.co.caprica.vlcj.binding.RuntimeUtil;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
-
-import com.sun.jna.Native;
-import com.sun.jna.NativeLibrary;
 
 /**
  * @author Debmalya
@@ -23,20 +17,10 @@ public class Tutorial {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			File vlcInstallPath = new File(args[0]);
-			
-			NativeLibrary.addSearchPath(
-	                RuntimeUtil.getLibVlcLibraryName(), vlcInstallPath.getAbsolutePath());
-//			Native.register(RuntimeUtil.getLibVlcLibraryName());
-//			Native.register(vlcInstallPath.getAbsolutePath());
-	        
-			// MediaPlayerFactory factory = new MediaPlayerFactory();
-			EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-		}else{
-			
-		}
-			
+		MediaPlayerFactory factory = new MediaPlayerFactory();
+		EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+		System.out.println("DONE");
+		
 	}
 
 }
