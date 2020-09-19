6cb4571ed506b220ddd7a3a3232eaffb792a9247
Debmalya Jash <debmalya.jash@gmail.com>
2020-09-19 12:41:14 +0800

    Trying to solve dll file loading issues in windows.

--- src/main/java/org/deb/Tutorial.java
+++ src/main/java/org/deb/Tutorial.java
@@ -3,16 +3,40 @@
  */
 package org.deb;
 
+import java.io.File;
+
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.binding.RuntimeUtil;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
 
 /**
  * @author Debmalya
  *
  */
 public class Tutorial {
-	public static void main(String[] args){
-//		MediaPlayerFactory factory = new MediaPlayerFactory();
-		EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+	/**
+	 * First argument is the path to libvlc.dll
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			File vlcInstallPath = new File(args[0]);
+			
+			NativeLibrary.addSearchPath(
+	                RuntimeUtil.getLibVlcLibraryName(), vlcInstallPath.getAbsolutePath());
+//			Native.register(RuntimeUtil.getLibVlcLibraryName());
+//			Native.register(vlcInstallPath.getAbsolutePath());
+	        
+			// MediaPlayerFactory factory = new MediaPlayerFactory();
+			EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+		}else{
+			
+		}
+			
 	}
 
 }
