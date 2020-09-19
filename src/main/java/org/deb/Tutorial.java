587add28efdd5aa04c434c1da9292b013489bd59
Debmalya Jash <debmalya.jash@gmail.com>
2020-09-19 23:49:44 +0800

    Introducing Panel

--- src/main/java/org/deb/Tutorial.java
+++ src/main/java/org/deb/Tutorial.java
@@ -3,10 +3,15 @@
  */
 package org.deb;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
@@ -20,6 +25,12 @@ public class Tutorial {
 
 	private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 
+	private final JButton pauseButton;
+
+	private final JButton rewindButton;
+
+	private final JButton skipButton;
+
 	/**
 	 * 
 	 * 
@@ -30,14 +41,15 @@ public class Tutorial {
 			Tutorial thisApp = new Tutorial();
 			thisApp.mediaPlayerComponent.mediaPlayer().media().play(args[0]);
 		} else {
-            System.err.println("Usage: java Tutorial <media file name with path>");     
+			System.err
+					.println("Usage: mvn compile exec:java -Dexec.mainClass=\"org.deb.Tutorial\" -Dexec.args=\"media\\aaj-tomare-dekhte-elem.mp3\"");
 		}
 
 	}
 
 	public Tutorial() {
-
-		frame = new JFrame("My First Media Player");
+		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+		frame = new JFrame("Media Player Tutorial");
 		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -49,8 +61,29 @@ public class Tutorial {
 			}
 		});
 
-		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-		frame.setContentPane(mediaPlayerComponent);
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new BorderLayout());
+		
+		
+		JPanel controlsPane = new JPanel();
+		
+		pauseButton = new JButton("Pause");
+		controlsPane.add(pauseButton);
+		pauseButton.addActionListener(e -> mediaPlayerComponent.mediaPlayer().controls().pause());
+		
+		rewindButton = new JButton("Rewind");
+		controlsPane.add(rewindButton);
+		rewindButton.addActionListener(e -> mediaPlayerComponent.mediaPlayer().controls().skipPosition(10.00f));
+		
+	
+		skipButton = new JButton("Skip");
+		controlsPane.add(skipButton);
+		skipButton.addActionListener(e->mediaPlayerComponent.mediaPlayer().controls().skipPosition(-10.00f));
+		contentPane.add(controlsPane, BorderLayout.SOUTH);
+
+		
+		contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
+		frame.setContentPane(contentPane);
 
 		frame.setVisible(true);
 
